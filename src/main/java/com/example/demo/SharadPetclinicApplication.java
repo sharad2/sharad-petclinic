f86bdfb80a4c5a82a2a6988dc6d3e09442ca1c2c
Sharad Singhal <happysharad@icloud.com>
2018-02-08 13:51:34 +0530

    Login with facebook works but logout misbehaves

--- src/main/java/com/example/demo/SharadPetclinicApplication.java
+++ src/main/java/com/example/demo/SharadPetclinicApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 //To make the link to Facebook we need an @EnableOAuth2Sso annotation on our main class:
@@ -17,13 +18,11 @@ public class SharadPetclinicApplication extends WebSecurityConfigurerAdapter {
 	
 	 @Override
 	  protected void configure(HttpSecurity http) throws Exception {
-	    http
-	      .antMatcher("/**")
-	      .authorizeRequests()
-	        .antMatchers("/", "/login**", "/webjars/**", "/resources/**")
-	        .permitAll()
-	      .anyRequest()
-	        .authenticated();
+			// @formatter:off
+			http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**", "/resources/**").permitAll().anyRequest()
+					.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
 	  }
 
 }
